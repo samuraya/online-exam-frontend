69378f03494e8c1237f2625064257d277d433ccf
samuraya <variskhanov@yahoo.com>
2020-10-29 16:00:23 +0700

    m

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,13 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
+
+  // module.exports = {
+  //   baseUrl: '/index.html/'
+  // }
+
+
+
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
