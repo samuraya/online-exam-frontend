b1819091deb6802d603f3891666d957f283b6755
samuraya <variskhanov@yahoo.com>
2020-10-31 12:38:42 +0700

    s

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+var history = require('connect-history-api-fallback');
 
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 
+app.use(history());
 app.use('/api', createProxyMiddleware({ target: 'https://online-exam-mfu-api.herokuapp.com', changeOrigin: true }));
 
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
